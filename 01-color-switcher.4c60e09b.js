69e236e2acdb2850f433afa5f737d97268f287b3
victoralexandrovich <129540913+victoralexandrovich@users.noreply.github.com>
2026-08-19 00:36:08 +0000

    Deploying to gh-pages from @ victoralexandrovich/goit-js-hw-09-2023@fe7b21e73b2bcf52c67a4cae9cc26298c2c53b73 🚀

---
+++ 01-color-switcher.4c60e09b.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let a=null;t.addEventListener("click",(function(){t.disabled=!0,a=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.4c60e09b.js.map
